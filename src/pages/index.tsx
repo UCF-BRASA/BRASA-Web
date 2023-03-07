87a2cd286486ab80926a14253209eadc2a428156
gdacosta14 <gccarvalho1499@gmail.com>
2023-03-06 19:17:34 -0500

    Testing commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
 
   return (
       <main className='h-screen bg-bgbeige'>
-        <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Welcome to BRASA</h1>
+        <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Welcome to BRASA!!</h1>
       </main>
   )
 }
