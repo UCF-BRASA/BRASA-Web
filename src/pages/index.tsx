9b7844fc6c7c3062be4dda4b0632886b76e976da
gdacosta14 <gccarvalho1499@gmail.com>
2023-03-06 19:22:43 -0500

    escreve uma coisinha

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ const Home: NextPage = () => {
   return (
       <main className='h-screen bg-bgbeige'>
         <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Welcome to BRASA!!</h1>
+        <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Facheti eu te amo!!</h1>
       </main>
   )
 }
