715b6a01da9e6df316ac98a4936e089d874bee83
Pedro Fachetti Carvalhom <pfachetti.pc@gmail.com>
2023-08-05 00:18:46 -0700

    BRWEB-19 & BRWEB-20 & BRWEB-21: Desktop style done

---
+++ src/pages/events.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Events: NextPage = () => {
+  return (
+    <main className="h-screen bg-bgbeige">
+      <h1 className="p-4 mb-4 text-5xl font-bold text-center text-red-600">Events Page</h1>
+    </main>
+  );
+};
+
+export default Events;
