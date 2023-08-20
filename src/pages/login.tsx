b7b0abc5fc1f562195abb02810f123298f26dbaa
Pedro Fachetti Carvalhom <pfachetti.pc@gmail.com>
2023-08-20 15:04:56 -0700

    Added cors headers (think it will work)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -35,13 +35,9 @@ const SignIn: NextPage<Props> = ({ isMobile }) => {
     };
 
     try {
-      const extraHeader = DEBUG_MODE
-        ? "http://localhost:3000"
-        : "https://brasa-web-git-development-ucf-brasa.vercel.app/";
+      const extraHeader = DEBUG_MODE ? "http://localhost:3000" : "https://brasaucf.com";
 
-      const apiDomain = DEBUG_MODE
-        ? "http://localhost:8080"
-        : "https://brasa-web-git-development-ucf-brasa.vercel.app/";
+      const apiDomain = DEBUG_MODE ? "http://localhost:8080" : "https://brasaucf.com";
       const endpoint = "/api/v0.1/auth/login";
       const url = apiDomain + endpoint; // Replace with your API endpoint URL
 
