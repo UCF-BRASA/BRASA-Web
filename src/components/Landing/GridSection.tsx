ab43484b79949cb84fb81e6878ca0d01b1e41863
Nascfn <cs.paulo.fn@gmail.com>
2025-03-31 19:25:58 -0400

    Home Page updates

--- src/components/Landing/GridSection.tsx
+++ src/components/Landing/GridSection.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const GridSection: FC<Props> = ({ imageAlt, imagePath, title, description }) => {
   return (
-    <div className="flex flex-col justify-between items-center text-center h-[12rem]">
+    <div className="flex flex-col justify-between items-center text-center h-[15rem]">
       {/* with flex-wrap use: mx-14 my-14 */}
       {/* these are SVGs, no need to use Next Image component */}
       <img src={imagePath} alt={imageAlt} loading="lazy" />
-      <h1 className="font-bold text-2xl">{title}</h1>
-      <p className="font-medium text-base text-black/60 w-auto break-normal">{description}</p>
+      <h1 className="font-bold text-3xl">{title}</h1>
+      <p className="font-large text-base text-black/60 w-auto break-normal">{description}</p>
     </div>
   );
 };
